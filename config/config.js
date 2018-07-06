6f05757022293b6051f31c575db2cf5c8727c060
wuhao <wuhaoxcp@gmail.com>
2018-07-06 16:22:53 +0800

    config.js删除没使用的代码

--- config/config.js
+++ config/config.js
@@ -8,13 +8,9 @@ module.exports = {
     devtool: 'inline-source-map',
   },
   build: {
-    index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/dist/',//相对于服务器路径
-    productionSourceMap: true,
     devtool: 'source-map',
-    productionGzip: false,
-    productionGzipExtensions: ['js', 'css'],
   }
 }
