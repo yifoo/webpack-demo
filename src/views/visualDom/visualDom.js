5a23a601bb8ada81d77f5c310aeef14dd708d670
wuhao <wuhaoxcp@gmail.com>
2018-11-14 22:17:54 +0800

    调整渲染

--- src/views/visualDom/visualDom.js
+++ src/views/visualDom/visualDom.js
@@ -2,26 +2,30 @@
  * @Author: wuhao 
  * @Date: 2018-11-13 15:52:42 
  * @Desc: 了解虚拟DOM
- * @Last Modified by:   wuhao 
- * @Last Modified time: 2018-11-13 15:52:42 
+ * @Last Modified by: wuhao
+ * @Last Modified time: 2018-11-14 12:54:29
  */
 /**
  * 获取节点对象
  */
 class vNode {
-  constructor({ tag, children, text }){
-    this.tag = tag
+  constructor({tagName,props,children}){
+    this.tagName = tagName
+    this.props = props
     this.children = children
-    this.text = text
   }
   render() {
-    if (this.tag === '#text') {
-      return document.createTextNode(this.text)
+    var el = document.createElement(this.tagName)
+    var props = this.props
+    // 遍历节点属性并设置
+    for(var propName in props){
+      el.setAttribute(propName,props[propName])
     }
-    // 元素都挂载到tag下
-    let el = document.createElement(this.tag)
-    this.children.map((vChild) => {
-      el.appendChild(new vNode(vChild).render())
+    var children = this.children || []
+    children.forEach((child)=>{
+      // 如果子节点是对象，递归构建DOM节点
+      var chileEle =typeof(child)==='object'?  new vNode(child).render() : document.createTextNode(child)// 如果字符串，只构建文本节点
+      el.appendChild(chileEle)
     })
     return el
   }
@@ -38,35 +42,18 @@ export default class VisualDom {
     this.visualRender()
   }
   visualRender() {
-    let nodesData = {
-      tag: 'div',
-      children: [
-        {
-          tag: 'div',
-          children: [
-            {
-              tag: 'span',
-              children: [
-                {
-                  tag: '#text',
-                  text: 'hello'
-                }
-              ]
-            }
-          ]
-        },
-        {
-          tag: 'span',
-          children: [
-            {
-              tag: '#text',
-              text: 'world'
-            }
-          ]
-        }
+    var element = {
+      tagName: 'ul', // 节点标签名
+      props: { // DOM的属性，用一个对象存储键值对
+        id: 'list'
+      },
+      children: [ // 该节点的子节点
+        {tagName: 'li', props: {class: 'item'}, children: ["Item 1"]},
+        {tagName: 'li', props: {class: 'item'}, children: ["Item 2"]},
+        {tagName: 'li', props: {class: 'item'}, children: ["Item 3"]},
       ]
     }
-    let node = new vNode(nodesData)
-    this.$el.find('.mount').html(node.render())
+    let node = new vNode(element).render()
+    this.$el.find('.mount').html(node)
   }
 }
